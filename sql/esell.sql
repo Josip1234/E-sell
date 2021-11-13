bf4fc917eb54e7092884632fb4ec23067c77f2c3
Josip Bošnjak <jbosnjak@unipu.hr>
2021-11-13 13:18:58 +0100

    added sql for database

---
+++ sql/esell.sql
@@ -0,0 +1,362 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 13, 2021 at 01:17 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `esell`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ad_details`
+--
+
+CREATE TABLE `ad_details` (
+  `id` int(11) NOT NULL,
+  `article_num` bigint(20) NOT NULL,
+  `ad_added` datetime NOT NULL,
+  `ad_last_updated` datetime NOT NULL,
+  `time_until_expire` varchar(100) COLLATE utf8_croatian_ci DEFAULT NULL,
+  `ad_shown` varchar(255) COLLATE utf8_croatian_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `articles`
+--
+
+CREATE TABLE `articles` (
+  `id` int(11) NOT NULL,
+  `article_number` bigint(20) NOT NULL,
+  `article_name` varchar(255) COLLATE utf8_croatian_ci NOT NULL,
+  `seller` varchar(50) COLLATE utf8_croatian_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `article_basic_details`
+--
+
+CREATE TABLE `article_basic_details` (
+  `id` int(11) NOT NULL,
+  `ar_num` bigint(20) NOT NULL,
+  `type_id` int(11) NOT NULL,
+  `price` varchar(50) COLLATE utf8_croatian_ci NOT NULL,
+  `article_condition` varchar(50) COLLATE utf8_croatian_ci DEFAULT NULL,
+  `type_id_2` int(11) DEFAULT NULL,
+  `type_id_3` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `article_types`
+--
+
+CREATE TABLE `article_types` (
+  `id` int(11) NOT NULL,
+  `type` varchar(255) COLLATE utf8_croatian_ci NOT NULL,
+  `types_of_categories` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci COMMENT='will be used by other table to get  type of products';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category_or_subcategory`
+--
+
+CREATE TABLE `category_or_subcategory` (
+  `id` int(11) NOT NULL,
+  `type` varchar(50) COLLATE utf8_croatian_ci NOT NULL,
+  `lang` varchar(2) COLLATE utf8_croatian_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sellers`
+--
+
+CREATE TABLE `sellers` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(50) COLLATE utf8_croatian_ci NOT NULL,
+  `lname` varchar(50) COLLATE utf8_croatian_ci NOT NULL,
+  `dateofbirth` date NOT NULL,
+  `location` text COLLATE utf8_croatian_ci NOT NULL,
+  `nickname` varchar(50) COLLATE utf8_croatian_ci NOT NULL,
+  `contact` varchar(150) COLLATE utf8_croatian_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8_croatian_ci NOT NULL,
+  `hash_password` longtext COLLATE utf8_croatian_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seller_information`
+--
+
+CREATE TABLE `seller_information` (
+  `id` int(11) NOT NULL,
+  `nickname` varchar(50) COLLATE utf8_croatian_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shipping_details`
+--
+
+CREATE TABLE `shipping_details` (
+  `id` int(11) NOT NULL,
+  `article_number` bigint(20) NOT NULL,
+  `shipping` varchar(255) COLLATE utf8_croatian_ci NOT NULL,
+  `comment` varchar(255) COLLATE utf8_croatian_ci DEFAULT NULL,
+  `location` varchar(255) COLLATE utf8_croatian_ci NOT NULL,
+  `delivery` varchar(255) COLLATE utf8_croatian_ci DEFAULT NULL,
+  `returns` varchar(100) COLLATE utf8_croatian_ci DEFAULT NULL,
+  `payments` varchar(255) COLLATE utf8_croatian_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shoe_details`
+--
+
+CREATE TABLE `shoe_details` (
+  `id` int(11) NOT NULL,
+  `article_number` bigint(20) NOT NULL,
+  `Shoe_condition` text COLLATE utf8_croatian_ci NOT NULL,
+  `Theme` varchar(20) COLLATE utf8_croatian_ci DEFAULT NULL,
+  `Pattern` varchar(20) COLLATE utf8_croatian_ci DEFAULT NULL,
+  `Type` varchar(255) COLLATE utf8_croatian_ci NOT NULL,
+  `Customized` tinyint(1) DEFAULT NULL,
+  `Color` varchar(20) COLLATE utf8_croatian_ci NOT NULL,
+  `Upper Material` varchar(20) COLLATE utf8_croatian_ci NOT NULL,
+  `Vintage` tinyint(1) DEFAULT NULL,
+  `Insole Material` varchar(20) COLLATE utf8_croatian_ci DEFAULT NULL,
+  `Occasion` text COLLATE utf8_croatian_ci DEFAULT NULL,
+  `Brand` varchar(50) COLLATE utf8_croatian_ci DEFAULT NULL,
+  `Shoe size` varchar(100) COLLATE utf8_croatian_ci NOT NULL,
+  `Lining Material` varchar(30) COLLATE utf8_croatian_ci DEFAULT NULL,
+  `Shoe_character` varchar(255) COLLATE utf8_croatian_ci NOT NULL,
+  `Accents` varchar(50) COLLATE utf8_croatian_ci DEFAULT NULL,
+  `Model` varchar(50) COLLATE utf8_croatian_ci DEFAULT NULL,
+  `Toe Shape` varchar(20) COLLATE utf8_croatian_ci DEFAULT NULL,
+  `Department` varchar(20) COLLATE utf8_croatian_ci DEFAULT NULL,
+  `Style` varchar(50) COLLATE utf8_croatian_ci DEFAULT NULL,
+  `Heel Style` varchar(30) COLLATE utf8_croatian_ci DEFAULT NULL,
+  `Shoe Shaft Style` varchar(30) COLLATE utf8_croatian_ci DEFAULT NULL,
+  `Season` varchar(100) COLLATE utf8_croatian_ci DEFAULT NULL,
+  `Outsole Material` varchar(50) COLLATE utf8_croatian_ci DEFAULT NULL,
+  `Heel Height` varchar(30) COLLATE utf8_croatian_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ad_details`
+--
+ALTER TABLE `ad_details`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `article_num` (`article_num`);
+
+--
+-- Indexes for table `articles`
+--
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `article_number` (`article_number`),
+  ADD KEY `seller` (`seller`),
+  ADD KEY `seller_2` (`seller`);
+
+--
+-- Indexes for table `article_basic_details`
+--
+ALTER TABLE `article_basic_details`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `ar_num` (`ar_num`),
+  ADD KEY `type_id` (`type_id`),
+  ADD KEY `type_id_2` (`type_id_2`),
+  ADD KEY `type_id_3` (`type_id_3`);
+
+--
+-- Indexes for table `article_types`
+--
+ALTER TABLE `article_types`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `type` (`type`),
+  ADD KEY `types_of_categories` (`types_of_categories`) USING BTREE;
+
+--
+-- Indexes for table `category_or_subcategory`
+--
+ALTER TABLE `category_or_subcategory`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `type` (`type`),
+  ADD KEY `lang` (`lang`);
+
+--
+-- Indexes for table `sellers`
+--
+ALTER TABLE `sellers`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nickname` (`nickname`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `seller_information`
+--
+ALTER TABLE `seller_information`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nickname` (`nickname`);
+
+--
+-- Indexes for table `shipping_details`
+--
+ALTER TABLE `shipping_details`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `article_number` (`article_number`);
+
+--
+-- Indexes for table `shoe_details`
+--
+ALTER TABLE `shoe_details`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `article_number` (`article_number`),
+  ADD KEY `Type` (`Type`),
+  ADD KEY `Shoe_character` (`Shoe_character`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `ad_details`
+--
+ALTER TABLE `ad_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `article_basic_details`
+--
+ALTER TABLE `article_basic_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `article_types`
+--
+ALTER TABLE `article_types`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `category_or_subcategory`
+--
+ALTER TABLE `category_or_subcategory`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sellers`
+--
+ALTER TABLE `sellers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `seller_information`
+--
+ALTER TABLE `seller_information`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `shipping_details`
+--
+ALTER TABLE `shipping_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `shoe_details`
+--
+ALTER TABLE `shoe_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `ad_details`
+--
+ALTER TABLE `ad_details`
+  ADD CONSTRAINT `article_number_fk` FOREIGN KEY (`article_num`) REFERENCES `articles` (`article_number`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `articles`
+--
+ALTER TABLE `articles`
+  ADD CONSTRAINT `seller_fk` FOREIGN KEY (`seller`) REFERENCES `seller_information` (`nickname`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `article_basic_details`
+--
+ALTER TABLE `article_basic_details`
+  ADD CONSTRAINT `article_id_fk` FOREIGN KEY (`ar_num`) REFERENCES `articles` (`article_number`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `article_type_fk` FOREIGN KEY (`type_id`) REFERENCES `article_types` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `type_id_2_fk` FOREIGN KEY (`type_id_2`) REFERENCES `article_types` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `type_id_3_fk` FOREIGN KEY (`type_id_3`) REFERENCES `article_types` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `article_types`
+--
+ALTER TABLE `article_types`
+  ADD CONSTRAINT `category_or_subcategory_fk` FOREIGN KEY (`types_of_categories`) REFERENCES `category_or_subcategory` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Constraints for table `seller_information`
+--
+ALTER TABLE `seller_information`
+  ADD CONSTRAINT `seller_inf_fk` FOREIGN KEY (`nickname`) REFERENCES `sellers` (`nickname`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `shipping_details`
+--
+ALTER TABLE `shipping_details`
+  ADD CONSTRAINT `art_num_fk` FOREIGN KEY (`article_number`) REFERENCES `articles` (`article_number`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `shoe_details`
+--
+ALTER TABLE `shoe_details`
+  ADD CONSTRAINT `an_fk` FOREIGN KEY (`article_number`) REFERENCES `articles` (`article_number`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `shoe_character` FOREIGN KEY (`Shoe_character`) REFERENCES `article_types` (`type`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `type_fk` FOREIGN KEY (`Type`) REFERENCES `article_types` (`type`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
