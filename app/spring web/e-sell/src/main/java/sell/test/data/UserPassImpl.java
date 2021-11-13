1cb0f1514c5877c24f2306aa1024c2037e45b278
Josip Bošnjak <jbosnjak@unipu.hr>
2021-11-13 15:27:34 +0100

    json data can now be saved

--- app/spring web/e-sell/src/main/java/sell/test/data/UserPassImpl.java
+++ app/spring web/e-sell/src/main/java/sell/test/data/UserPassImpl.java
@@ -1,6 +1,8 @@
 package sell.test.data;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map;
 
 import org.json.JSONArray;
@@ -20,6 +22,14 @@ public class UserPassImpl implements UserPassword{
 		JSONArray data_array=createJsonArray(data, sellers);
 		System.out.println(data_array);
 	   
+		  try (FileWriter file = new FileWriter("src/main/resources/static/test-data.json")) {
+	            //We can write any JSONArray or JSONObject instance to the file
+	            file.write(data_array.toString()); 
+	         
+	 
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
 	   
 	   
 		return false;
