3c4a573d822ac08579398cc0a88f85d5142b9c0e
Josip Bošnjak <jbosnjak3@gmail.com>
2022-01-23 15:49:05 +0100

    modified php functions
    modified php function for use on other unique values for unique value validation

--- php/functions.php
+++ php/functions.php
@@ -32,5 +32,44 @@ function makeFirstPart($value){
 function printString($value){
 	echo $value;
 }
+function returnNumberOfrecords($table){
+	include("sql_connect.php");	
+	$countSql="SELECT COUNT(*) AS total FROM $table";
+	$cnt=mysqli_query($dbc,$countSql);
+	$resOfCount=mysqli_fetch_assoc($cnt);
+	$totalValues=$resOfCount['total'];
+	return $totalValues;
+	}
+
+	function selectUniqueFromTables($tables,$uniqueValue){
+		include("sql_connect.php");
+		$sql1= "SELECT $uniqueValue FROM $tables";
+		$result=mysqli_query($dbc,$sql1);
+		return $result;
+	}
+
+	function createJsonFromUniques($result,$totalValues,$tableColumn){
+		$concatString=makeFirstPart($tableColumn);
+		$count=0;
+		$valueKey="value";
+	while($row=mysqli_fetch_array($result)){
+		$count=$count+1;
+		if($count==$totalValues){
+				$concatString.=openBracket1();
+			 $concatString.=addValueToKey($valueKey,$row[$tableColumn]);
+			$concatString.=closeBracket1();
+			$concatString.=closeBracket2();
+		}else{
+		$concatString.=openBracket1();
+		   $concatString.=addValueToKey($valueKey,$row[$tableColumn]);
+			$concatString.=closeBracket1();
+			$concatString.=getComma();
+		}
+		
+	}
+	$concatString.=closeBracket1();
+		return $concatString;
+	}
+
 
 ?>
--- php/nicknames_from_sellers.php
+++ php/nicknames_from_sellers.php
@@ -2,53 +2,10 @@
 include("functions.php");
 
 
-function selectNicknamesFromSellers(){
-	include("sql_connect.php");
-	$sql1= "SELECT `nickname` FROM `sellers`;";
-    $result=mysqli_query($dbc,$sql1);
-	return $result;
-}
-
-function returnNumberOfrecordsFromSellers(){
-include("sql_connect.php");	
-$countSql="SELECT COUNT(*) AS total FROM `sellers`;";
-$cnt=mysqli_query($dbc,$countSql);
-$resOfCount=mysqli_fetch_assoc($cnt);
-$totalValues=$resOfCount['total'];
-return $totalValues;
-}
-
-
-function createJsonFromNicknames($result,$totalValues){
-	$concatString=makeFirstPart("nickname");
-	$count=0;
-	$valueKey="value";
-while($row=mysqli_fetch_array($result)){
-	$count=$count+1;
-	if($count==$totalValues){
-			$concatString.=openBracket1();
-	     $concatString.=addValueToKey($valueKey,$row['nickname']);
-		$concatString.=closeBracket1();
-		$concatString.=closeBracket2();
-	}else{
-	$concatString.=openBracket1();
-	   $concatString.=addValueToKey($valueKey,$row['nickname']);
-		$concatString.=closeBracket1();
-		$concatString.=getComma();
-	}
-	
-}
-$concatString.=closeBracket1();
-	return $concatString;
-}
-
-$result=selectNicknamesFromSellers();
-$totalValues=returnNumberOfrecordsFromSellers();
-$json=createJsonFromNicknames($result,$totalValues);
+$result=selectUniqueFromTables("sellers","nickname");
+$totalValues=returnNumberOfrecords("sellers");
+$json=createJsonFromUniques($result,$totalValues,"nickname");
 printString($json);
 	
-
-
-
 
 ?>
