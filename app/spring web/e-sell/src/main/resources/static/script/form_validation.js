2c08a0c7bf55c3067de1e4d92405bf0e2f0aac24
Josip Bošnjak <jbosnjak3@gmail.com>
2022-01-23 16:43:41 +0100

    fixed bug
    fixed some bug with adding incomplete variable to the form with data validation

--- app/spring web/e-sell/src/main/resources/static/script/form_validation.js
+++ app/spring web/e-sell/src/main/resources/static/script/form_validation.js
@@ -3,12 +3,16 @@ var app=angular.module('formValidation',[]);
 app.config(function($sceDelegateProvider) {
               $sceDelegateProvider.resourceUrlWhitelist([
                 'http://localhost/e-sell/php/nicknames_from_sellers.php',
-                'http://localhost:8080/e-sell/en/registration'
+                'http://localhost:8080/e-sell/en/registration',
+				'http://localhost/e-sell/php/email_from_sellers.php'
               ]);
             });
 app.controller('validateForm', function($scope,$http){
 	$scope.data=$http.get("http://localhost/e-sell/php/nicknames_from_sellers.php").then(function(response){
         $scope.myData = response.data.nickname;
+    });
+	$scope.emails=$http.get("http://localhost/e-sell/php/email_from_sellers.php").then(function(response){
+        $scope.emailData = response.data.email;
     });
 	$scope.fname=document.getElementById("fname").value;
 	$scope.lname=document.getElementById("lname").value;
@@ -19,6 +23,7 @@ app.controller('validateForm', function($scope,$http){
 	$scope.email=document.getElementById("email").value;
 	$scope.hash_password=document.getElementById("hash_password").value;
 	$scope.error=false;
+	$scope.incomplete=true;
 	$scope.$watch('fname', function(){$scope.validate();})
 	$scope.$watch('lname', function(){$scope.validate();})
 	$scope.$watch('dateofbirth', function(){$scope.validate();})
@@ -38,11 +43,20 @@ app.controller('validateForm', function($scope,$http){
 			$scope.alreradyExists=false;
 		}
 		
+		if($scope.emailData.some(em => em.value === $scope.email)==true){
+			$scope.error=true;
+			$scope.alreradyExistsEmail=true;	
+		  }else{
+			  $scope.error=false;
+			  $scope.alreradyExistsEmail=false;
+		  }
+		  
 		
 		
 	   	if($scope.fname.length==0 || $scope.lname.length==0 || $scope.dateofbirth.length==0 || $scope.location.length==0 || $scope.nickname.length==0 || $scope.contact.length==0 || $scope.email.length==0 || $scope.hash_password.length==0){
 		$scope.error=true;
 		$scope.showError=true;
+		$scope.incomplete=true;
 	   }else{
 		//just in case first part do not work
 		if($scope.myData.some(code => code.value === $scope.nickname)==true){
@@ -52,6 +66,14 @@ app.controller('validateForm', function($scope,$http){
 			$scope.error=false;
 			$scope.alreradyExists=false;
 		}
+			if($scope.emailData.some(em => em.value === $scope.email)==true){
+			$scope.error=true;
+			$scope.alreradyExistsEmail=true;	
+		  }else{
+			  $scope.error=false;
+			  $scope.alreradyExistsEmail=false;
+		  }
+		  $scope.incomplete=false;
 		
 	}
 	};
