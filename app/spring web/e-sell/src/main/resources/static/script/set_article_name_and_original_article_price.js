dd42ab93cf7f5a463773ab8b7c4452b670a545ec
Josip Bošnjak <jbosnjak3@gmail.com>
2022-02-06 15:18:31 +0100

    update of article details
    added clas sinstead id for each one added javascript functions need to determine hot to get value from price in outise json

---
+++ app/spring web/e-sell/src/main/resources/static/script/set_article_name_and_original_article_price.js
@@ -0,0 +1,26 @@
+var app=angular.module('articleBasic',[]);
+
+app.config(function($sceDelegateProvider) {
+              $sceDelegateProvider.resourceUrlWhitelist([
+                'http://localhost/e-sell/php/article_basic_prices.php',
+                'http://localhost/e-sell/php/article_names_numbers.php'
+              ]);
+            });
+app.controller('articleBasicDetails', function($scope,$http){
+	$scope.data=$http.get("http://localhost/e-sell/php/article_names_numbers.php").then(function(response){
+        $scope.myData = response.data.Article;
+    });
+	$scope.emails=$http.get("http://localhost/e-sell/php/article_basic_prices.php").then(function(response){
+        $scope.emailData = response.data.Article;
+    });
+	var article_numbers=document.getElementsByClassName("article_number");
+	
+for (var i = 0; i < article_numbers.length; i++) {
+    var article=article_numbers[i].innerHTML;
+    	alert(article);
+    	alert($scope.myData.some(code => code.article_number));
+    if($scope.myData.some(code => code.article_number === article)==true){
+	alert(article);
+}
+}
+});
