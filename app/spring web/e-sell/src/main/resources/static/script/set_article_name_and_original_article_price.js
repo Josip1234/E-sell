6a9fa3ba757d159da50fc6146fe2282dcdd739ae
Josip Bošnjak <jbosnjak3@gmail.com>
2022-02-08 16:03:17 +0100

    we have changed article id to article names

--- app/spring web/e-sell/src/main/resources/static/script/set_article_name_and_original_article_price.js
+++ app/spring web/e-sell/src/main/resources/static/script/set_article_name_and_original_article_price.js
@@ -7,17 +7,34 @@ app.config(function($sceDelegateProvider) {
               ]);
             });
 app.controller('articleBasicDetails', function($scope,$http){
-	var article_numbers=document.getElementsByClassName("article_number");
+	
 	$scope.data=$http.get("http://localhost/e-sell/php/article_names_numbers.php").then(function(response){
         $scope.article = response.data.article;
-        //need to see how to access data outside this scope
-          $scope.ar=$scope.article[3].article_number;
-          for (var i = 0; i < article_numbers.length; i++) {
-		var an = article_numbers[i].innerHTML;
-	    var bn=$scope.article[i].article_number;
-      alert(bn);
+        $scope.article_numbers=document.getElementsByClassName("article_number");
+        
 		
+          for (var i = 0; i < $scope.article.length; i++) {
+	         var value1=$scope.article[i].article_number;
+	         
+	         for (var j = 0; j < $scope.article_numbers.length; j++) {
+		          
+		          var value2=$scope.article_numbers[j].innerHTML;
+		          var valueToWrite=$scope.article[i].article_name;
+	         if(value1===value2){
+		var index=j;
+		alert(value2);
+		alert(index);
+		document.getElementById(value2).innerHTML=valueToWrite;
+		
+		}
+	         
+	         
 	}
+		   
+		   
+		};
+	
+      
       
     });
 	$scope.emails=$http.get("http://localhost/e-sell/php/article_basic_prices.php").then(function(response){
