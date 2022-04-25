a33a7acbded4f6581a399929d33d4814ab5d16bf
Josip Bošnjak <jbosnjak3@gmail.com>
2022-04-25 21:28:00 +0200

    update
    need initial data because of validation

--- app/spring web/e-sell/src/main/resources/db/migration/V1.1__create_tables.sql
+++ app/spring web/e-sell/src/main/resources/db/migration/V1.1__create_tables.sql
@@ -1,5 +1,3 @@
-
-
 CREATE TABLE `ad_details` (
   `id` int(11) NOT NULL,
   `article_num` varchar(20) COLLATE utf8_croatian_ci NOT NULL,
@@ -7,6 +5,14 @@ CREATE TABLE `ad_details` (
   `ad_last_updated` datetime NOT NULL,
   `time_until_expire` varchar(100) COLLATE utf8_croatian_ci DEFAULT NULL,
   `ad_shown` varchar(255) COLLATE utf8_croatian_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
+
+
+CREATE TABLE `articles` (
+  `id` int(11) NOT NULL,
+  `article_number` varchar(20) COLLATE utf8_croatian_ci NOT NULL,
+  `article_name` varchar(255) COLLATE utf8_croatian_ci NOT NULL,
+  `seller` varchar(50) COLLATE utf8_croatian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
 
 
@@ -21,13 +27,10 @@ CREATE TABLE `article_basic_details` (
   `type_id_3` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
 
-
-
 CREATE TABLE `article_types` (
   `id` int(11) NOT NULL,
   `type` varchar(255) COLLATE utf8_croatian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci COMMENT='will be used by other table to get  type of products';
-
 
 
 CREATE TABLE `car_detail` (
@@ -54,6 +57,7 @@ CREATE TABLE `car_detail` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
 
 
+
 CREATE TABLE `sellers` (
   `id` int(11) NOT NULL,
   `fname` varchar(50) COLLATE utf8_croatian_ci NOT NULL,
@@ -77,7 +81,6 @@ CREATE TABLE `shipping_details` (
   `returns` varchar(100) COLLATE utf8_croatian_ci DEFAULT NULL,
   `payments` varchar(255) COLLATE utf8_croatian_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
-
 
 
 CREATE TABLE `shoe_details` (
@@ -107,3 +110,112 @@ CREATE TABLE `shoe_details` (
   `Outsole Material` varchar(50) COLLATE utf8_croatian_ci DEFAULT NULL,
   `Heel Height` varchar(30) COLLATE utf8_croatian_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
+
+
+ALTER TABLE `ad_details`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `article_num` (`article_num`);
+
+
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `article_number` (`article_number`),
+  ADD KEY `seller` (`seller`),
+  ADD KEY `seller_2` (`seller`);
+
+
+ALTER TABLE `article_basic_details`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `ar_num` (`ar_num`),
+  ADD KEY `type_id` (`type_id`),
+  ADD KEY `type_id_2` (`type_id_2`),
+  ADD KEY `type_id_3` (`type_id_3`);
+
+
+ALTER TABLE `article_types`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `type` (`type`);
+
+
+ALTER TABLE `car_detail`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `article_number` (`article_number`);
+
+
+ALTER TABLE `sellers`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nickname` (`nickname`),
+  ADD UNIQUE KEY `email` (`email`);
+
+
+ALTER TABLE `shipping_details`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `article_number` (`article_number`);
+
+
+ALTER TABLE `shoe_details`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `article_number` (`article_number`),
+  ADD KEY `Shoe_character` (`Shoe_character`);
+
+
+
+ALTER TABLE `ad_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+
+ALTER TABLE `articles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+
+ALTER TABLE `article_basic_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+
+ALTER TABLE `article_types`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+
+ALTER TABLE `car_detail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+
+ALTER TABLE `sellers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+
+ALTER TABLE `shipping_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+
+ALTER TABLE `shoe_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+
+ALTER TABLE `ad_details`
+  ADD CONSTRAINT `article_number_fk` FOREIGN KEY (`article_num`) REFERENCES `articles` (`article_number`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+
+ALTER TABLE `articles`
+  ADD CONSTRAINT `seller_fk` FOREIGN KEY (`seller`) REFERENCES `sellers` (`nickname`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+
+ALTER TABLE `article_basic_details`
+  ADD CONSTRAINT `article_id_fk` FOREIGN KEY (`ar_num`) REFERENCES `articles` (`article_number`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `article_type_fk` FOREIGN KEY (`type_id`) REFERENCES `article_types` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `type_id_2_fk` FOREIGN KEY (`type_id_2`) REFERENCES `article_types` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `type_id_3_fk` FOREIGN KEY (`type_id_3`) REFERENCES `article_types` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+
+ALTER TABLE `car_detail`
+  ADD CONSTRAINT `an2_fk` FOREIGN KEY (`article_number`) REFERENCES `articles` (`article_number`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+
+ALTER TABLE `shipping_details`
+  ADD CONSTRAINT `art_num_fk` FOREIGN KEY (`article_number`) REFERENCES `articles` (`article_number`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+
+ALTER TABLE `shoe_details`
+  ADD CONSTRAINT `an_fk` FOREIGN KEY (`article_number`) REFERENCES `articles` (`article_number`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `shoe_character` FOREIGN KEY (`Shoe_character`) REFERENCES `article_types` (`type`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
