376a899b773e08de7c789f305ca83aef950f22a5
Josip Bošnjak <jbosnjak3@gmail.com>
2022-06-01 20:33:43 +0200

    created interface
    added map double double to class numeric. Created definition of functions which are needed to separate maximum value to classes.

---
+++ app/spring web/e-sell/src/main/java/sell/functions/Classification.java
@@ -0,0 +1,16 @@
+package sell.functions;
+
+import java.util.List;
+import java.util.Map;
+
+public interface Classification {
+  public Double divideInHalf(Double number);
+  public Double subtractNumber(Double number,Double with);
+  public Double rememberRemainder(Double number);
+  public Map<Double,Double> addToMap(Double key,Double value);
+  public List<Map<Double,Double>> addMapToList();
+  public boolean checkDecimalValue(double number);
+  public double rememberNewMaximum(double number);
+  public boolean isLast(int count, int listLength);
+  public boolean isFirst(int count);
+}
--- app/spring web/e-sell/src/main/java/sell/functions/Numeric.java
+++ app/spring web/e-sell/src/main/java/sell/functions/Numeric.java
@@ -1,5 +1,7 @@
 package sell.functions;
 
+import java.util.Map;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Numeric {
- public double key;
- public double value;
+ public Double key;
+ public Double value;
+ public Double remainder;
+ public Map<Double, Double> classification;
 }
