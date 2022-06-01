354d41ff8d0c05c66e9e4bf16b22d17c8b9d892a
Josip Bošnjak <jbosnjak3@gmail.com>
2022-06-01 20:19:33 +0200

    Create Numeric.java
    added class numeric for parsing key values from double double from  map double double which will be used to classified price ranges

---
+++ app/spring web/e-sell/src/main/java/sell/functions/Numeric.java
@@ -0,0 +1,13 @@
+package sell.functions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Numeric {
+ public double key;
+ public double value;
+}
