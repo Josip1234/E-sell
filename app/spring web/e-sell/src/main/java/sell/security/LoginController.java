8188ae6ef91c07eb883970126aa9a57e87e94e38
Josip Bošnjak <jbosnjak3@gmail.com>
2022-07-02 10:47:18 +0200

    login error is working now

--- app/spring web/e-sell/src/main/java/sell/security/LoginController.java
+++ app/spring web/e-sell/src/main/java/sell/security/LoginController.java
@@ -1,7 +1,9 @@
 package sell.security;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 @Controller
@@ -11,6 +13,7 @@ public class LoginController {
 	public String getLogin() {
 		return "login";
 	}
+
 
 	public void addViewControllers(ViewControllerRegistry registry) {
 	
