352ef4f4cba4622aa56ac30a8f13dccca176a1f6
Josip Bošnjak <jbosnjak3@gmail.com>
2022-07-25 20:40:04 +0200

    Update Article_advanced_details.java
    Removed values from shoe class added new ones

--- app/spring web/e-sell/src/main/java/sell/articles/types/Article_advanced_details.java
+++ app/spring web/e-sell/src/main/java/sell/articles/types/Article_advanced_details.java
@@ -11,30 +11,9 @@ import lombok.Data;
 @AllArgsConstructor
 public class Article_advanced_details {
 private Integer id;
-private String article_number;
-private String Shoe_condition;
-private String Theme;
-private String Pattern;
-private boolean Customized;
-private String Color;
-private String Upper_Material;
-private boolean Vintage;
-private String Insole_Material;
-private String Occasion;
-private String Brand;
-private String Shoe_size;
-private String Lining_Material;
-private String Shoe_character;
-private String Accents;
-private String Model;
-private String Toe_Shape;
-private String Department;
-private String Style;
-private String Heel_Style;
-private String Shoe_Shaft_Style;
-private String Season;
-private String Outsole_Material;
-private String Heel_Height;
+private String article_num;
+private String description1;
+private String description2;
 
 
 }
