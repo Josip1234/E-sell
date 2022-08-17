1130425eb5190b79d96ec8a26752fa8b1a6e9583
Josip Bošnjak <jbosnjak3@gmail.com>
2022-08-17 20:10:42 +0200

    Create Storage.java

---
+++ app/spring web/e-sell/src/main/java/sell/files/Storage.java
@@ -0,0 +1,19 @@
+package sell.files;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Storage {
+	private String nickname;
+	private String article_number;
+	private String user_folder;
+	private String article_folder;
+	private String file_name;
+	private String file_extension;
+	private String local_path;
+	private String url;
+}
