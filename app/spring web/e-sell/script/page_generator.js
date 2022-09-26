de6e72ad253e94e67dbe63940bf5dde94e5f230e
Josip Bošnjak <jbosnjak3@gmail.com>
2022-09-26 19:55:33 +0200

    Update page_generator.js
    added function for generation of table

--- app/spring web/e-sell/script/page_generator.js
+++ app/spring web/e-sell/script/page_generator.js
@@ -1,25 +1,62 @@
-var table="<table>";
-var thead="<thead>";
-var row="<tr>";
-var header="<th>";
-var header2="</th>";
-var row2="</tr>";
-var thead2="</thead>";
-var tbody="<tbody>";
-var td="<td>";
-var td2="</td>";
-var tbody2="</tbody>";
-var table2="</table>";
+let table="<table>";
+let thead="<thead>";
+let row="<tr>";
+let header="<th>";
+let header2="</th>";
+let row2="</tr>";
+let thead2="</thead>";
+let tbody="<tbody>";
+let td="<td>";
+let td2="</td>";
+let tbody2="</tbody>";
+let table2="</table>";
 
-var ol="<ol>";
-var li="<li>";
-var li2="</li>";
-var ol2="</ol>";
+let ol="<ol>";
+let li="<li>";
+let li2="</li>";
+let ol2="</ol>";
 
-var ul="<ul>";
-var ul2="</ul>";
+let ul="<ul>";
+let ul2="</ul>";
 
-var p="<p>";
-var strong="<strong>";
-var strong2="</strong>";
-var p2="</p>";
+let p="<p>";
+let strong="<strong>";
+let strong2="</strong>";
+let p2="</p>";
+
+
+function generate_table(number_of_rows, number_of_headers, data){
+let generate_table_header="";
+let generate_table_body="";
+let table="";
+generate_table_header=generateHeaderOrBody("header",number_of_headers,data);
+generate_table_body=generateHeaderOrBody("body",number_of_rows,data);
+table+=table;
+table+=thead;
+table+=row;
+table+=generate_table_header;
+table+=row2;
+table+=thead2;
+table+=tbody;
+table+=generate_table_body;
+table+=tbody2;
+table+=table2;
+return table;
+}
+
+function generateHeaderOrBody(header_or_body, number, data){
+    let table_element="";
+    if(header_or_body=="header"){
+       for (let index = 0; index < number; index++) {
+            table_element+=header+data+header2;
+       }
+    }else if(header_or_body=="body"){
+        for (let index = 0; index < number; index++) {
+            table_element+=row+td+data+td2+row2;
+            
+        }
+    }else{
+        header_or_body="";
+    }
+    return table_element;
+}
