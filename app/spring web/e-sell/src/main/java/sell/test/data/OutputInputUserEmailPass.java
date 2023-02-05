cebf21ba42e8835425049d63c9fe9e0fcbce49f6
Josip Bošnjak <jbosnjak3@gmail.com>
2023-02-05 16:13:12 +0100

    Update OutputInputUserEmailPass.java
    added another button for saving data

--- app/spring web/e-sell/src/main/java/sell/test/data/OutputInputUserEmailPass.java
+++ app/spring web/e-sell/src/main/java/sell/test/data/OutputInputUserEmailPass.java
@@ -10,10 +10,18 @@ import javax.swing.GroupLayout.Alignment;
 import java.awt.SystemColor;
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class OutputInputUserEmailPass extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField username;
+	private JTextField password;
 
 	/**
 	 * Launch the application.
@@ -48,20 +56,69 @@ public class OutputInputUserEmailPass extends JFrame {
 		JLabel lblNewLabel = new JLabel("Welcome to the application. To load data press load button.");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 24));
 		lblNewLabel.setBackground(SystemColor.menu);
+		
+		JButton dload = new JButton("Load data");
+		dload.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		dload.setBackground(SystemColor.info);
+		
+		JTextArea loadedData = new JTextArea();
+		loadedData.setBackground(SystemColor.info);
+		
+		username = new JTextField();
+		username.setBackground(SystemColor.info);
+		username.setColumns(10);
+		
+		password = new JTextField();
+		password.setBackground(SystemColor.info);
+		password.setColumns(10);
+		
+		JButton saveData = new JButton("Save data");
+		saveData.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		saveData.setBackground(SystemColor.info);
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(85)
-					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 740, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(17, Short.MAX_VALUE))
+					.addContainerGap(295, Short.MAX_VALUE)
+					.addComponent(dload, GroupLayout.PREFERRED_SIZE, 271, GroupLayout.PREFERRED_SIZE)
+					.addGap(310))
+				.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+					.addGap(273)
+					.addComponent(saveData, GroupLayout.PREFERRED_SIZE, 263, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(340, Short.MAX_VALUE))
+				.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+					.addGap(78)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(loadedData, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 740, Short.MAX_VALUE)
+						.addComponent(lblNewLabel, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 740, Short.MAX_VALUE)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(username, GroupLayout.PREFERRED_SIZE, 325, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addComponent(password, GroupLayout.DEFAULT_SIZE, 397, Short.MAX_VALUE)))
+					.addGap(58))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addContainerGap()
 					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 105, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(651, Short.MAX_VALUE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(dload, GroupLayout.PREFERRED_SIZE, 62, GroupLayout.PREFERRED_SIZE)
+					.addGap(35)
+					.addComponent(loadedData, GroupLayout.PREFERRED_SIZE, 329, GroupLayout.PREFERRED_SIZE)
+					.addGap(56)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(username, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+						.addComponent(password, GroupLayout.PREFERRED_SIZE, 34, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addComponent(saveData, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(46, Short.MAX_VALUE))
 		);
 		contentPane.setLayout(gl_contentPane);
 	}
