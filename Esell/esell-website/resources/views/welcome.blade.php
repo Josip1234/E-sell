d8e9777754bb7269dfebda85522854d15e71a692
Josip1234 <jbosnjak3@gmail.com>
2026-08-25 13:28:17 +0200

    Update welcome.blade.php
    added additional section at homepage it will print products with most views

--- Esell/esell-website/resources/views/welcome.blade.php
+++ Esell/esell-website/resources/views/welcome.blade.php
@@ -53,7 +53,7 @@
             @endif
 
         </header>
-          <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+          <h2 class="font-semibold text-xl text-gray-800 leading-tight mb-2">
             {{ __('Recently added products') }}
         </h2>
         <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
@@ -144,6 +144,95 @@
                 </div>
                 </div>
 
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight mb-2">
+            {{ __('Products with most views') }}
+        </h2>
+                <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
+                             <div class="max-w-sm bg-white border border-gray-200 rounded-2xl shadow-md overflow-hidden mb-2">
+                    @php
+                       //algorithm
+                       $image="";
+                       $imgToGet="uploads/index8.jpg";
+                       $images = File::allFiles('uploads');
+                       foreach ($images as $value) {
+                         $strReplace=str_replace("\\","/",$value);
+
+                         if($imgToGet===$strReplace){
+                             $image=$strReplace;
+                            break;
+                         }
+                       }
+                    @endphp
+                    <!-- Card Image -->
+                    <img class="w-full h-48 object-cover" src="{{ $image }}" alt="Cover">
+                    <!-- Card Body -->
+                    <div class="p-5">
+                        <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900">
+                            Card title
+                        </h5>
+                        <p class="mb-4 text-gray-600">Here are the biggest enterprise technology acquisitions of 2021.</p>
+                        <a href="#" class="inline-flex items-center px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">
+                            Read more
+                        </a>
+                    </div>
+                </div>
+                                    <div class="max-w-sm bg-white border border-gray-200 rounded-2xl shadow-md overflow-hidden mb-2">
+                    @php
+                       //algorithm
+                       $image="";
+                       $imgToGet="uploads/index8.jpg";
+                       $images = File::allFiles('uploads');
+                       foreach ($images as $value) {
+                         $strReplace=str_replace("\\","/",$value);
+
+                         if($imgToGet===$strReplace){
+                             $image=$strReplace;
+                            break;
+                         }
+                       }
+                    @endphp
+                    <!-- Card Image -->
+                    <img class="w-full h-48 object-cover" src="{{ $image }}" alt="Cover">
+                    <!-- Card Body -->
+                    <div class="p-5">
+                        <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900">
+                            Card title
+                        </h5>
+                        <p class="mb-4 text-gray-600">Here are the biggest enterprise technology acquisitions of 2021.</p>
+                        <a href="#" class="inline-flex items-center px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">
+                            Read more
+                        </a>
+                    </div>
+                </div>
+                                    <div class="max-w-sm bg-white border border-gray-200 rounded-2xl shadow-md overflow-hidden mb-2">
+                    @php
+                       //algorithm
+                       $image="";
+                       $imgToGet="uploads/index8.jpg";
+                       $images = File::allFiles('uploads');
+                       foreach ($images as $value) {
+                         $strReplace=str_replace("\\","/",$value);
+
+                         if($imgToGet===$strReplace){
+                             $image=$strReplace;
+                            break;
+                         }
+                       }
+                    @endphp
+                    <!-- Card Image -->
+                    <img class="w-full h-48 object-cover" src="{{ $image }}" alt="Cover">
+                    <!-- Card Body -->
+                    <div class="p-5">
+                        <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900">
+                            Card title
+                        </h5>
+                        <p class="mb-4 text-gray-600">Here are the biggest enterprise technology acquisitions of 2021.</p>
+                        <a href="#" class="inline-flex items-center px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">
+                            Read more
+                        </a>
+                    </div>
+                </div>
+                </div>
 
         </div>
 
